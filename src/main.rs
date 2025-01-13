f2c41ff79b23188ff62efac530509de299d1279e
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2025-01-13 14:22:34 +0100

    Example Usage to sync TUM to Google Calendar

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use dotenv::dotenv;
+use ics_watcher::{print_events, tum_google_sync, ICSWatcher};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    // TODO:
+    // * Expects und unwraps bubblen
+
+    dotenv().ok();
+
+    let tum_url = env::var("TUM_URL").expect("TUM_URL not found in environment");
+    let google_calendar_id =
+        env::var("GOOGLE_CALENDAR_ID").expect("GOOGLE_CALENDAR_ID not found in environment");
+
+    let mut ics_watcher = ICSWatcher::new(
+        tum_url.as_str(),
+        vec![
+            Box::new(|a, b, e| Box::pin(async move { print_events(a, b, e).await })),
+            Box::new(move |a, b, e| {
+                let calendar_id = google_calendar_id.clone();
+                Box::pin(async move { tum_google_sync(&calendar_id, a, b, e).await })
+            }),
+        ],
+    );
+
+    // Try to load backup
+    let _ = ics_watcher.load_backup("TUM Calendar");
+    ics_watcher
+        .run(Option::from("TUM Calendar"))
+        .await
+        .expect("ICS Watcher crashed");
+}
